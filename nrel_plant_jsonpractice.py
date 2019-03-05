ba34017308e8a786deee34291f5ac0d8c8d72ad6
tahentx <hendricks.ta@gmail.com>
2019-03-05 15:09:56 -0800

    practice working with json data using nrel data set. still more work to do

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -2,4 +2,10 @@ import json
 with open('nrel-pv_plants.json') as json_file:
     plants = json.load(json_file)
 
-print(plants)
+
+# count = plants["features"].count("projname")
+# print(count)
+sites = []
+for i in plants:
+    sites.append(plants["features"][0]["properties"])
+print(sites)
