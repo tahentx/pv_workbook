b0b5769b9c69a0ecf1158e77e41ede7a86ed6424
tahentx <hendricks.ta@gmail.com>
2019-03-05 16:12:55 -0800

    remove nested lookup import

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,11 +1,4 @@
 import json
 with open('nrel-pv_plants.json') as json_file:
-    plants = json.load(json_file)
-
-
-# count = plants["features"].count("projname")
-# print(count)
-sites = []
-for i in plants:
-    sites.append(plants["features"][0]["properties"])
-print(sites)
+    data = json.load(json_file)
+print(data)
