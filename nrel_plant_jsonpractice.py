546be01f1d6a304353cd041911857086aefecf63
tahentx <hendricks.ta@gmail.com>
2019-03-07 07:21:19 -0800

    access the first attribute in the first dictionary of the features list

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,4 +1,7 @@
 import json
 with open('nrel-pv_plants.json') as json_file:
     data = json.load(json_file)
-print(data)
+
+features = data["features"]
+linden = data["features"][0].get("type")
+print(linden)
