f312607d470e661cdf50c030a17d5d68a50a1713
tahentx <hendricks.ta@gmail.com>
2019-03-07 07:34:48 -0800

    access the first project name in features list and print the name

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -3,5 +3,6 @@ with open('nrel-pv_plants.json') as json_file:
     data = json.load(json_file)
 
 features = data["features"]
-linden = data["features"][0].get("type")
-print(linden)
+linden = data["features"][0]["properties"].get("projname")
+linden_developer = data["features"][0]["properties"].get("developer")
+print("The first project in the NREL database is " + str(linden))
