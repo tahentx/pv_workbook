ab5321e2122a802bb7924132c4abfb3f5984f448
tahentx <hendricks.ta@gmail.com>
2019-03-07 18:36:05 -0800

    first iteration of the for loop. attempting to loop through json object to find developers

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,8 +1,9 @@
 import json
 with open('nrel-pv_plants.json') as json_file:
     data = json.load(json_file)
-
-features = data["features"]
 linden = data["features"][0]["properties"].get("projname")
-linden_developer = data["features"][0]["properties"].get("developer")
-print("The first project in the NREL database is " + str(linden))
+developer = data["features"][0]["properties"].get("developer")
+site = data["features"][0]["properties"]
+for site in site.items():
+    developer = data["features"][0]["properties"].get("developer")
+    print(developer)
