5158f2926187858459d3f2f16f6bd32f14d1e012
tahentx <hendricks.ta@gmail.com>
2019-03-09 10:01:21 -0800

    experimenting with capturing data within JSON object

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,10 +1,16 @@
 import json
 with open('nrel-pv_plants.json') as json_file:
     data = json.load(json_file)
-site = list(data["features"][0]["properties"])
-test = data["features"][0:]
-if "developer" in site:
-    print("yes")
+fleet = data["features"][0]["properties"]
+site = data["features"][0]["properties"]["projname"]
+
+fleet_list = data["features"]
+for project in site:
+    print(project)
+#
+# for developer in fleet:
+#     if "developer" in fleet:
+#         print("test")
 
 # for site in site.items():
 #     print(site)
