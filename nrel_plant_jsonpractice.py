bbe5de83f5e3bae6a1ce6c227221101b5df663bc
tahentx <hendricks.ta@gmail.com>
2019-03-09 09:11:12 -0800

    convert the features dictionary to a list for easier traversing

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,9 +1,10 @@
 import json
 with open('nrel-pv_plants.json') as json_file:
     data = json.load(json_file)
-linden = data["features"][0]["properties"].get("projname")
-developer = data["features"][0]["properties"].get("developer")
-site = data["features"][0]["properties"]
-for site in site.items():
-    developer = data["features"][0]["properties"].get("developer")
-    print(developer)
+site = list(data["features"][0]["properties"])
+test = data["features"][0:]
+if "developer" in site:
+    print("yes")
+
+# for site in site.items():
+#     print(site)
