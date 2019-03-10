5e6d7d304942bf59f4bd2319ca7dfd6e668cd84d
tahentx <hendricks.ta@gmail.com>
2019-03-10 08:51:13 -0700

    convert site variable back to dictionary from string

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,16 +1,7 @@
 import json
 with open('nrel-pv_plants.json') as json_file:
     data = json.load(json_file)
-fleet = data["features"][0]["properties"]
-site = data["features"][0]["properties"]["projname"]
-
 fleet_list = data["features"]
-for project in site:
-    print(project)
-#
-# for developer in fleet:
-#     if "developer" in fleet:
-#         print("test")
-
-# for site in site.items():
-#     print(site)
+site = data["features"][0]["properties"]
+for site in site.items():
+    print(site)
