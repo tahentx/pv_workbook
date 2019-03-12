3f27e85d3e25543d2b1c60a204db9163382bf8c4
tahentx <hendricks.ta@gmail.com>
2019-03-12 06:42:55 -0700

    print sunpower projects

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -2,4 +2,7 @@ import json
 with open('data.json') as json_file:
     data = json.load(json_file)
 for feature in data['features']:
-    print(feature['properties']['projname'])
+    if feature['properties']['developer'] == "SunPower":
+        print(feature['properties']['projname'])
+
+    # print(feature['properties']['projname'])
