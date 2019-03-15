c9d10393842b17b8ef5ca1acc9e0e91e312525ba
tahentx <hendricks.ta@gmail.com>
2019-03-14 21:19:59 -0700

    print california plants

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -5,8 +5,8 @@ with open('data.json') as json_file:
 #     if feature['properties']['developer'] == "SunPower":
 #         print(feature['properties']['projname'])
 ca = []
+print("The California projects: ")
 for feature in data['features']:
     if feature['properties']['state'] == "CA":
         ca.append(feature)
-
-print("The California projects: ")
+        print(feature['properties']['projname'] + " City: " + feature['properties']['city_cnty'] + " State: " + feature['properties']['state'])
