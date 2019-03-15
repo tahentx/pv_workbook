796ac3ef858312e2272d8115467b85f5818f0508
tahentx <hendricks.ta@gmail.com>
2019-03-14 21:50:38 -0700

    start attempt at alphebetical

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,12 +1,22 @@
 import json
 with open('data.json') as json_file:
     data = json.load(json_file)
+
+# print project name
 # for feature in data['features']:
 #     if feature['properties']['developer'] == "SunPower":
 #         print(feature['properties']['projname'])
+
+# print california projects
+# ca = []
+# print("The California projects: ")
+# for feature in data['features']:
+#     if feature['properties']['state'] == "CA":
+#         ca.append(feature)
+#         print(feature['properties']['projname'] + " City: " + feature['properties']['city_cnty'] + " State: " + feature['properties']['state'])
+
 ca = []
-print("The California projects: ")
 for feature in data['features']:
-    if feature['properties']['state'] == "CA":
+    if feature['properties']['developer'] == "SunPower":
         ca.append(feature)
-        print(feature['properties']['projname'] + " City: " + feature['properties']['city_cnty'] + " State: " + feature['properties']['state'])
+        print(feature['properties']['projname'])
