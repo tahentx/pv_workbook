5cdfad6435c730eb7fd20d1e5daefbe6c5223730
tahentx <hendricks.ta@gmail.com>
2019-03-14 20:39:03 -0700

    append california projects to empty list

--- nrel_plant_jsonpractice.py
+++ nrel_plant_jsonpractice.py
@@ -1,8 +1,12 @@
 import json
 with open('data.json') as json_file:
     data = json.load(json_file)
+# for feature in data['features']:
+#     if feature['properties']['developer'] == "SunPower":
+#         print(feature['properties']['projname'])
+ca = []
 for feature in data['features']:
-    if feature['properties']['developer'] == "SunPower":
-        print(feature['properties']['projname'])
+    if feature['properties']['state'] == "CA":
+        ca.append(feature)
 
-    # print(feature['properties']['projname'])
+print("The California projects: ")
