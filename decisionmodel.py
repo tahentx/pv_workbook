6be058892a762b8458b835615a3959e9eb32b70d
tahentx <hendricks.ta@gmail.com>
2019-03-19 22:19:16 -0700

    earned value calculation using example data

--- decisionmodel.py
+++ decisionmodel.py
@@ -1,3 +1,5 @@
 import pydecisions as pyd
-a = pyd.fin()
-print(a.npv(.3,[-100,50,30,20,10]))
+# a = pyd.fin()
+# print(a.npv(.3,[-100,50,30,20,10]))
+a = pyd.evm(100,0.5,0.4,45)
+print(a.results())
