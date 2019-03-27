7f77d4193d5fcd726ff1edb36fc4e65a15b1eec5
tahentx <hendricks.ta@gmail.com>
2019-03-27 16:43:18 -0700

    test present value calculation

--- decisionmodel.py
+++ decisionmodel.py
@@ -1,5 +1,11 @@
 import pydecisions as pyd
 # a = pyd.fin()
-# print(a.npv(.3,[-100,50,30,20,10]))
-a = pyd.evm(100,0.5,0.4,45)
-print(a.results())
+# cashflows = [-100,50,30,20,10]
+# print(a.npv(.3,cashflows))
+
+earned_value = pyd.evm(100,0.5,0.4,45)
+print(earned_value.results())
+
+
+# a = pyd.evm(100,0.5,0.4,45)
+# print(a.results())
