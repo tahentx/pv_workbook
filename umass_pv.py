b4842c13f530e1a722d940d74048c29609d42701
tahentx <hendricks.ta@gmail.com>
2019-04-14 17:25:51 -0700

    one of the first really helpful exploratory data analysis commands: describe. command returns summary statistics on columns that have integers

--- umass_pv.py
+++ umass_pv.py
@@ -4,12 +4,15 @@ data = pd.read_csv('umass.csv')
 df = pd.DataFrame(data)
 
 # print the first ten lines of the df
-print(df.head(10))
+# print(df.head(10))
 # print shape of data
-print(df.shape)
+# print(df.shape)
 
 # print columns
-print(df.columns)
+# print(df.columns)
 
 # print general Information about the dataset
-print(df.info())
+# print(df.info())
+
+# run summary statistics on data
+print(df.describe())
