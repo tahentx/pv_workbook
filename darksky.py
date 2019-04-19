e770ebefed94a70df3715402c9da1a50529b0c53
tahentx <hendricks.ta@gmail.com>
2019-04-19 15:51:49 -0700

    modify the url string to exclude sections of response data. unsuccessful.

--- darksky.py
+++ darksky.py
@@ -1,17 +1,14 @@
 import requests
 import json
 
-# dpath might be the library to use to pull data from complex dictionary
-# url and api key to test solaredge api
-
-
 def main():
+	print("\n Dark Sky API tool \n")
 	lat = input('latitude:')
 	long = input('longitude:')
 	api_key = 'e56fa72ef66e61dd4ce85c58c6cc55aa'
-	url = "https://api.darksky.net/forecast/" + str(api_key) + "/" + str(lat) + "," + str(long)
+	url = "https://api.darksky.net/forecast/" + str(api_key) + "/" + str(lat) + "," + str(long) 
 	response = requests.get(url)
-	print(response.text)
+	print((response.text))
 
 
 if __name__ == "__main__":
