8e0eabdf975eacbb2133e751e455b236d37f6cce
tahentx <hendricks.ta@gmail.com>
2019-04-19 15:50:46 -0700

    beginning of data wrangling exercise

--- wrangling.py
+++ wrangling.py
@@ -3,8 +3,9 @@ a7 = pd.read_csv('a7_plantview.csv', index_col = 0)
 df = pd.DataFrame(a7)
 
 # function that exports data to json
-def export(df):
-    print("Exporting data to JSON")
-    with open('a7.json', 'w') as f:
-        f.write(df.to_json(orient='records', lines=True))
-export(df)
+# def export(df):
+#     print("Exporting data to JSON")
+#     with open('a7.json', 'w') as f:
+#         f.write(df.to_json(orient='records', lines=True))
+# export(df)
+print(df.info())
