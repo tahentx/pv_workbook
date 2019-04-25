e8bea442ce3c9d99c716abc102bea2969722a724
tahentx <hendricks.ta@gmail.com>
2019-04-25 15:53:23 -0700

    starting on codewars reduce exercise

---
+++ codewars_reduce.py
@@ -0,0 +1,7 @@
+# Given a non-empty array of integers, return the result of multiplying the values together in order.
+array = [5,5,2]
+def reduce(array):
+    for x in array:
+        print(x)
+
+reduce(array)
