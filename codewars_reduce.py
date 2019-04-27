99a06ab8e23bfe958537fb589f4943c7a113ca48
tahentx <hendricks.ta@gmail.com>
2019-04-26 18:08:13 -0700

    append the result variable to the empty array 'list'. print the last element of the list.

--- codewars_reduce.py
+++ codewars_reduce.py
@@ -1,9 +1,10 @@
 # Given a non-empty array of integers, return the result of multiplying the values together in order.
 ham = [5,5,2,6]
 def reduce(ham):
+    list = []
     result = 1
     for x in ham:
         result = result * x
-        print(result)
-
+        list.append(result)
+    print(list[-1])
 reduce(ham)
