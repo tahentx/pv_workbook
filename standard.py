5d99efc0d96b591960e4b3f53240ef92f369dc04
tahentx <hendricks.ta@gmail.com>
2019-05-10 10:26:46 -0700

    capture the difference between each observation and the mean. add each value to differences total list

--- standard.py
+++ standard.py
@@ -3,3 +3,11 @@ grades1 = [68, 71, 81, 90, 92]
 grades2 = [78, 83, 85, 85, 87]
 
 all_students = grades2 + grades1
+mean = sum(all_students) / len(all_students)
+differences_total = []
+
+for x in all_students:
+    diff = x - mean
+    differences_total.append(diff)
+
+print(differences_total)
