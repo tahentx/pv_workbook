0b5d52a0b18d4c333cef6d108ccf9a95933b723f
tahentx <hendricks.ta@gmail.com>
2019-05-10 11:48:17 -0700

    light formatting for cleanliness

--- standard.py
+++ standard.py
@@ -4,10 +4,19 @@ grades2 = [78, 83, 85, 85, 87]
 
 all_students = grades2 + grades1
 mean = sum(all_students) / len(all_students)
+
 differences_total = []
+def find_diff(all_students):
+    for x in all_students:
+        diff = x - mean
+        differences_total.append(diff)
 
-for x in all_students:
-    diff = x - mean
-    differences_total.append(diff)
+find_diff(all_students)
 
-print(differences_total)
+differences_total_sq = []
+def sq_diff(differences_total):
+    for x in differences_total:
+        sq = lambda x: x ** 2
+        differences_total_sq.append(sq(x))
+
+print(differences_total_sq)
