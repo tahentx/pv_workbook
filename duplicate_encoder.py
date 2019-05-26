6d80e055d4300900d6545f61d3fa2e018322f37c
tahentx <hendricks.ta@gmail.com>
2019-05-26 11:32:11 -0700

    import collections module

--- duplicate_encoder.py
+++ duplicate_encoder.py
@@ -1,5 +1,10 @@
+import collections
 word = input("Input word: ")
+# creating an empty list & appending the indices to the list makes iteration more manageable. the two lines below do that.
 placeholder = []
 for x in word:
     placeholder.append(x)
-print(placeholder)
+
+
+for x in placeholder:
+    print(x in placeholder)
