c7218b4fc2626fad17b3291b4f7a3c751759b531
tahentx <hendricks.ta@gmail.com>
2019-06-01 16:55:32 -0700

    repurposing this document for a new kata in codewars

--- codewars_reduce.py
+++ codewars_reduce.py
@@ -1,10 +1,5 @@
-# Given a non-empty array of integers, return the result of multiplying the values together in order.
-ham = [5,5,2,6]
-def reduce(ham):
-    list = []
-    result = 1
-    for x in ham:
-        result = result * x
-        list.append(result)
-    print(list[-1])
-reduce(ham)
+text = input("Test word:")
+def duplicate_count(text):
+    copy = text
+    print copy
+duplicate_count(text)
