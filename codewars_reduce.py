8cc3a077049a9fc18ae86c43529e3dd59c171361
tahentx <hendricks.ta@gmail.com>
2019-06-01 17:08:11 -0700

    capture duplicate values in the string

--- codewars_reduce.py
+++ codewars_reduce.py
@@ -1,5 +1,7 @@
 text = input("Test word:")
 def duplicate_count(text):
     copy = text
-    print copy
+    for x in copy:
+        if copy.count(x) > 1:
+            print(x + " has a duplicate value.")
 duplicate_count(text)
