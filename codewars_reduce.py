7084ac3d43b12238d4aba11cdd9edb748f245109
tahentx <hendricks.ta@gmail.com>
2019-06-01 17:27:13 -0700

    need to add the second condition to complete the exercise

--- codewars_reduce.py
+++ codewars_reduce.py
@@ -1,7 +1,6 @@
 text = input("Test word:")
 def duplicate_count(text):
-    copy = text
-    for x in copy:
-        if copy.count(x) > 1:
+    for x in text:
+        if text.count(x) > 1:
             print(x + " has a duplicate value.")
 duplicate_count(text)
