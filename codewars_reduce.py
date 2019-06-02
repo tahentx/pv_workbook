0a0beec3d594c498dc559610fea145d4f9e3b5ed
tahentx <hendricks.ta@gmail.com>
2019-06-02 15:58:10 -0700

    append dups, the empty list, in order to check for values that are upper case

--- codewars_reduce.py
+++ codewars_reduce.py
@@ -1,6 +1,9 @@
 text = input("Test word:")
+dups = []
 def duplicate_count(text):
     for x in text:
         if text.count(x) > 1:
+            dups.append(x)
             print(x + " has a duplicate value.")
+            print(dups)
 duplicate_count(text)
