16a1d074f1dfe83f730469c0e94184886ec22b7e
tahentx <hendricks.ta@gmail.com>
2019-06-08 15:54:22 -0700

    capture thesis from page 2391

---
+++ kimber_prototype.py
@@ -0,0 +1,1 @@
+# "Most existing PV system programs assume PV module soiling loss that is constant through time. Our observations of measured performance suggest that performance losses due to system soiling are not constant through time, rather they depend on the amount and frequency of rain that falls on the system"
