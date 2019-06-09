8cc00d3da6c3f2b0c5bf54df5cdfb6706cfb332d
tahentx <hendricks.ta@gmail.com>
2019-06-08 18:28:28 -0700

    capture user input and convert to list, manipulate, and then convert back to string

--- reverse_words.py
+++ reverse_words.py
@@ -1,3 +1,7 @@
 word = input("Give a word to flip: ")
-flip = word[::-1]
-print(flip)
+foo = word.split()
+swap = []
+for x in list(foo):
+    swap.append(x[::-1])
+final = ' '.join(swap)
+print(final)
