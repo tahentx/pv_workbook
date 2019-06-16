740e32d64a34af6ea127e9a0c7bf66969bf2abd0
tahentx <hendricks.ta@gmail.com>
2019-06-15 22:36:46 -0700

    convert solaredge output to dataframe

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import requests
-import json
+import pandas as pd
 
 # dpath might be the library to use to pull data from complex dictionary
 # url and api key to test solaredge api
 def main():
 	url = "https://monitoringapi.solaredge.com/site/581140/energy?timeUnit=QUARTER_OF_AN_HOUR&startDate=2018-10-23&endDate=2018-10-24&api_key=XUR0SMGADU7Y2NY4M2R7ZOS3KJ5WXSR4"
-	api_key = {'api_key': 'XGTHLV7EGP2EM1UTP7FWIY6PL9NKDT7W'}
+	api_key = {'api_key': '47WOTOHKC1QCXHSF101MG9DAI5GA52SH'}
 # first use of requests library
 	response = requests.get(url,params=api_key)
-	print(response.status_code)
+	payload = response.text
+	df = pd.DataFrame(eval(payload))
+	
+
+	# print(response.text)
 
 
 if __name__ == "__main__":
