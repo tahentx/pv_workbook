a4084695179f0f613eaa4f14cc85b47874b785d6
tahentx <hendricks.ta@gmail.com>
2019-06-17 13:39:15 -0700

    create empty list and append the distance value using haversine algorithm

--- metstationdistance.py
+++ metstationdistance.py
@@ -2,6 +2,11 @@ from haversine import haversine
 lyon = (45.7597, 4.8422)
 paris = (48.8567, 2.3508)
 esb = (40.748440, -73.985664)
-group = [lyon,paris,esb]
-for x in group:
-	print(haversine(lyon,x,unit='mi'))
+data = [lyon,paris,esb]
+
+backups = []
+for i in range(len(data)-1):
+	backups.append(haversine(data[i], data[i +1],unit='mi'))
+
+print(backups)
+print(min(backups))
