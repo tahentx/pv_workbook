6ddbac82beab0e0952ae44e1366154b5fa610514
tahentx <hendricks.ta@gmail.com>
2019-06-22 11:04:01 -0700

    define get the variance function

--- function_practice.py
+++ function_practice.py
@@ -21,4 +21,20 @@ def diff_from_mean(mean: int, observations: list) -> list:
     return differences
 
 y = diff_from_mean(avg, n)
-print(y)
+
+def observations_squared(distances_from_mean: list) -> list:
+    squared = []
+    for x in distances_from_mean:
+        sq = x ** 2
+        squared.append(sq)
+    sum_of_sq = sum(squared)
+    return sum_of_sq
+
+z = observations_squared(y)
+
+def get_the_variance(sq_sum,count):
+    variance = sq_sum / count
+    return variance
+
+v = get_the_variance(z,len(n))
+ 
