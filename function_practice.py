4926ddcc1efc57ab09c8e7142591971b7dcc7d52
tahentx <hendricks.ta@gmail.com>
2019-06-22 06:10:55 -0700

    writing out how to do a standard deviation the long way as a way to practice form, technique and defining functions properly; next step is sum of squares

--- function_practice.py
+++ function_practice.py
@@ -2,5 +2,23 @@ def return_sum(x,y):
     c = x + y
     return c
 
-res = return_sum(4,5)
-print(res)
+# res = return_sum(4,5)
+# print(res)
+
+n = [94,61,93,154,78,65,32]
+
+def get_mean(sample):
+    mean = sum(sample) / len(sample)
+    return mean
+
+avg = get_mean(n)
+
+def diff_from_mean(mean: int, observations: list) -> list:
+    differences = []
+    for x in observations:
+        difference = mean - x
+        differences.append(difference)
+    return differences
+
+y = diff_from_mean(avg, n)
+print(y)
