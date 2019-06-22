99a3578fc2e0b174a9ea9399b021c94d86cb1a52
tahentx <hendricks.ta@gmail.com>
2019-06-22 05:47:29 -0700

    removed logic from function and deleted function. commented out find_backup_metstation function since it still needs to be debugged. will do it after a little nap

--- metstationdistance.py
+++ metstationdistance.py
@@ -1,5 +1,6 @@
 import csv
 from haversine import haversine
+
 with open('tucson.csv') as file:
     has_header = csv.Sniffer().has_header(file.read(1024))
     file.seek(0)
@@ -7,11 +8,18 @@ with open('tucson.csv') as file:
     if has_header:
         next(met)
     met_list = list(met)
+
+
 coords = []
 for x in met_list:
     coords.append(x[1:])
-backup_mets = []
-for i in range(len(coords)-1):
-	backup_mets.append(haversine(coords[i], coords[i + 1],unit='mi'))
+
+print(coords)
 #
-print(backup_mets)
+# def find_backup_metstation(coordinates: list) -> list:
+#     backup_mets = []
+#     for i in range(len(coordinates)-1):
+#     	backup_mets.append(haversine(coordinates[i], coordinates[i + 1],unit='mi'))
+#     print(type(coordinates[i]))
+#
+# find_backup_metstation(coords)
