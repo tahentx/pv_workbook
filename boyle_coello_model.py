83c5b4856d2a73592c5c3e49fb74a3ddd37c9ff8
tahentx <hendricks.ta@gmail.com>
2019-06-26 17:30:15 -0700

    enumeration of model parameters

--- boyle_coello_model.py
+++ boyle_coello_model.py
@@ -1,5 +1,23 @@
 
-def boyle_coello_model(time=0,rain=0,rainthresh=,tilt=,pm2_5=,pm_10=,**kwargs):
-    kwargs = _build_kwargs([''])
+def boyle_coello_model(self,time,rain = 0,rainthreshold = 0,tilt = 0,pm2_5 = 0,pm_10=,**kwargs):
 
-    # modeltype, RainAccPeriod, LUC, Windspeed, Temperature
+    """
+    Use the :py:func:`boyle_coello_model` function to determine the impact of dirt accumulation on performance.
+
+    Parameters
+    ----------
+    time : numeric
+        Importing the datetime module would be suitable here
+
+    rain : array-like
+        Hourly rain accumulation values of the same duration defined in the time parameter. Units in milimeters.
+
+
+    Returns
+    -------
+    See pvsystem.calcparams_cec for details
+    """
+
+
+
+    kwargs = _build_kwargs([''])
