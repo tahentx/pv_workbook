54c3087515d55373ff46b01fe148a5d60d7d01d5
tahentx <hendricks.ta@gmail.com>
2019-07-01 14:25:25 -0700

    further define parameters to function

--- boyle_coello_model.py
+++ boyle_coello_model.py
@@ -8,10 +8,23 @@ def boyle_coello_model(self,time,rain = 0,rainthreshold = 0,tilt = 0,pm2_5 = 0,p
     ----------
     time : numeric
         Importing the datetime module would be suitable here
-
     rain : array-like
         Hourly rain accumulation values of the same duration defined in the time parameter. Units in milimeters.
+    rainthreshold : float
+        A scalar for the amount of rain in an accumulation period needed to clear the modules. In periods where the accumulated rain meets oro exceeds the threshold, the panels are assumed to be cleaned immediately after the accumulation period.
+    tilt : int
+        A scalar or vector for the tilt of the PV panels.
+    PM2_5 : float
+        The concentration of particulate matter with diamter less than 2.5 microns, in g/m^3.
+    PM10 : float
+        The concentration of particulate matter with diamter less than 10 microns, in g/m^3.
+    ModelType : int
+        Optional input to determine the model type to be used in the soiling model. A value of "1"
 
+    RainAccPeriod :
+        optional input that specifies the period, in hours
+        over which to accumulate rainfall totals before checking against the
+        rain cleaning threshold.
 
     Returns
     -------
