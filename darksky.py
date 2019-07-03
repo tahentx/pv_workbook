b7c08822b7f89d20a9d9a51173987c58e87926f5
tahentx <hendricks.ta@gmail.com>
2019-07-03 10:37:14 -0700

    add exclusion parameters to the url string to reduce output

--- darksky.py
+++ darksky.py
@@ -3,10 +3,13 @@ import json
 
 def main():
 	print("\n Dark Sky API tool \n")
-	lat = input('latitude:')
-	long = input('longitude:')
+	lat = 41.172508
+	long = -73.438858
 	api_key = 'e56fa72ef66e61dd4ce85c58c6cc55aa'
-	url = "https://api.darksky.net/forecast/" + str(api_key) + "/" + str(lat) + "," + str(long) 
+	time = '1559347200'
+	exclusion = "exclude="
+	ex1 = "currently"
+	url = "https://api.darksky.net/forecast/" + str(api_key) + "/" + str(lat) + "," + str(long) + "," + time + "?" + str(exclusion) + str(ex1)
 	response = requests.get(url)
 	print((response.text))
 
