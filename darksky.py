d7da0769f60cd3330f01c3049adb2b60185d3e19
tahentx <hendricks.ta@gmail.com>
2019-07-03 13:55:12 -0700

    pull json data and use pandas to write to excel

--- darksky.py
+++ darksky.py
@@ -1,8 +1,9 @@
 import requests
 import json
+import darksky
+import pandas as pd
 
 def main():
-	print("\n Dark Sky API tool \n")
 	lat = 41.172508
 	long = -73.438858
 	api_key = 'e56fa72ef66e61dd4ce85c58c6cc55aa'
@@ -11,8 +12,8 @@ def main():
 	ex1 = "currently"
 	url = "https://api.darksky.net/forecast/" + str(api_key) + "/" + str(lat) + "," + str(long) + "," + time + "?" + str(exclusion) + str(ex1)
 	response = requests.get(url)
-	print((response.text))
-
+	data = pd.read_json(path_or_buf=response.text,orient='index')
+	data.to_excel('test.xlsx',sheet_name="Sheet1")
 
 if __name__ == "__main__":
 		main()
