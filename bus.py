f4af006b48005ab4a4bee12b0e44d23da211fbfe
tahentx <hendricks.ta@gmail.com>
2019-07-04 13:56:37 -0700

    working solution for testing

--- bus.py
+++ bus.py
@@ -1,6 +1,7 @@
-# bus_stops = 5
-# riders = 6
+import random
+bus_stops = 5
 while bus_stops >= 0:
+    total = 10
     x = lambda a, b, c : a + b - c
-    print(x(5, 6, 2))
+    print(x(total, random.randint(0, 5), random.randint(2, 5)))
     bus_stops = bus_stops - 1
