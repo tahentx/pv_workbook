dffeef38ab172c2d7de0383c5ed652821695b53e
tahentx <hendricks.ta@gmail.com>
2019-07-04 21:05:51 -0700

    complete solution

--- bus.py
+++ bus.py
@@ -3,5 +3,6 @@ bus_stops = 5
 start = [random.randint(0, 10),0]
 while bus_stops >= 0:
     x = lambda a, b, c : a + b - c
-    print(x(sum(start), random.randint(0, 5), random.randint(2, 5)))
+    flow = x(sum(start), random.randint(0, 5), random.randint(2, 5))
+    return flow
     bus_stops = bus_stops - 1
