21321f877f7e56e04783f1692318c8722ca7a595
tahentx <hendricks.ta@gmail.com>
2019-07-05 18:01:23 -0700

    test ability to pull first entry. success.

--- psql.py
+++ psql.py
@@ -1,4 +1,6 @@
 import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
+conn = psycopg2.connect("host=localhost dbname=pv user=postgres password=N0rC@1Life!")
 cur = conn.cursor()
 cur.execute('SELECT * FROM metadata')
+first = cur.fetchone()
+print(first)
