82b42549b179f30bc0e3360578fea4d7f2011551
tahentx <hendricks.ta@gmail.com>
2019-07-09 16:52:10 -0700

    successfully add a record to postgres. next step is to uload csv file

--- psql.py
+++ psql.py
@@ -2,8 +2,6 @@ import psycopg2
 conn = psycopg2.connect("host=localhost dbname=pv user=postgres password=N0rC@1Life!")
 print("Connecting to database")
 cur = conn.cursor()
-with open(r'C:\Users\thendricks\pfdrivedev\pv_workbook\oasg.csv') as f:
-    next(f)
-    cur.copy_from(f,'metadata', sep=',')
-    conn.execute()
-    conn.close()
+insert_query = "INSERT INTO metadata VALUES {}".format("('999','Solar Mission III','2000.06.048','Vail Academy', '154.05', 'API', 'Also Energy', '7762 E Science  Park Dr', 'AZ', '85747', '32.09662', '-110.826', 'Tucson')")
+cur.execute(insert_query)
+conn.commit()
