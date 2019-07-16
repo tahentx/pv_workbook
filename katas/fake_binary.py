8658209d7a205f5d5a238c1dff68d68564dbef02
tahentx <hendricks.ta@gmail.com>
2019-07-15 21:45:14 -0700

    exercise complete

--- katas/fake_binary.py
+++ katas/fake_binary.py
@@ -2,12 +2,13 @@ def fake_bin(x: str) -> str:
     z = list(x)
     new = []
     for i in z:
-        if int(i) > 5:
+        if int(i) >= 5:
             i = "1"
             new.append(i)
         elif int(i) < 5:
             i = "0"
             new.append(i)
-    print(new)
+    output = ''.join(new)
+    print(output)
 
 fake_bin("43529347502")
