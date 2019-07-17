82acdaa0257a59ec74d8480601758b184f485a1f
tahentx <hendricks.ta@gmail.com>
2019-07-17 08:19:05 -0700

    first draft of list comprehension. need to revise

--- katas/dna.py
+++ katas/dna.py
@@ -1,7 +1,13 @@
 # Codewars kata: https://www.codewars.com/kata/dna-to-rna-conversion/train/python
 
-def DNAtoRNA(dna):
-    for i in dna:
-        print(i)
+def DNAtoRNA(dna: str) -> str:
 
-DNAtoRNA("blue")
+    """ Translate a given Deoxyribonucleic acid (DNA) to Ribonucleic acid (RNA) """
+    input = list(dna)
+    for x in input:
+        print(x)
+        if x == "T":
+            print("U")
+    # rna = ["U" for i in input if i == "T"]
+    # print(rna)
+DNAtoRNA("Time")
