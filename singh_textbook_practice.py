f396b6d2e4bd9a1e2fdfce5dd6214a15242c2c07
tahentx <hendricks.ta@gmail.com>
2019-07-31 17:11:26 -0700

    learned to do matrix multiplication. first attempt returned value error because i used * operand. used matmul and it worked.

--- singh_textbook_practice.py
+++ singh_textbook_practice.py
@@ -1,7 +1,20 @@
 import numpy as np
 
-# Matrix arithmetic, pg. 46
+# Matrix arithmetic, pg. 46, Example 1.17
 
 x = np.array([[1,2],[3,4]])
 y = np.array([[5,6],[7,8]])
 answer = x + y
+
+# Matrix arithmetic, pg. 46, Example 1.18
+
+A = np.array([[2,3],[-1,5]])
+x = np.array([2,1])
+answer = A * x
+
+# Matrix arithmetic, pg. 51, Example 1.19
+
+f =  np.array([[2,3,6],[1,5,7]])
+g =  np.array([[3,7],[4,2],[1,3]])
+h = np.matmul(f,g)
+print(h)
