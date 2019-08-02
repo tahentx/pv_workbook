9230cb79b12fac1c4cdf6bbdedac7bef08b2291c
tahentx <hendricks.ta@gmail.com>
2019-08-02 14:40:56 -0700

    test write to csv

--- pdfreader.py
+++ pdfreader.py
@@ -1,4 +1,11 @@
 import PyPDF2
+import csv
+
 pdfFileObj = open('ag.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages)
+pageObj = pdfReader.getPage(0)
+print(pageObj.extractText())
+
+contract = open('terms.csv','w', newline='')
+terms = csv.writer(contract)
+terms.writerow(['This','is','a','test'])
