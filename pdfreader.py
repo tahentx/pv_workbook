a683e547a8cecbbd99189ff7bfc1e96e6d230205
tahentx <hendricks.ta@gmail.com>
2019-08-02 16:00:14 -0700

    write to csv using pdftoexcel api

--- pdfreader.py
+++ pdfreader.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import pdftables_api
 import csv
 
 pdfFileObj = open('ag.pdf', 'rb')
@@ -9,3 +10,6 @@ print(pageObj.extractText())
 contract = open('terms.csv','w', newline='')
 terms = csv.writer(contract)
 terms.writerow(['This','is','a','test'])
+
+c = pdftables_api.Client('yu8wx1021116')
+c.csv('nrel_sample_agreement.pdf', 'terms')
