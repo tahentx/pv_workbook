419609e217c1d538f0781a70da81fd56f7d3f9c2
tahentx <hendricks.ta@gmail.com>
2019-08-13 16:13:20 -0700

    assign salesforce record id to all of the parts

--- spares.py
+++ spares.py
@@ -6,7 +6,12 @@ df = pd.DataFrame(
     {"Storeroom ID" : ["Service Truck 20","Service Truck 34"]}
 )
 
+# Assign CJ as default buyer
 for x in df:
     df["Default Buyer"] = 'CJ Kneringer'
 
-print(df)
+# Write data to CSV
+df.to_csv('storerooms.csv',sep='\t', encoding='utf-8')
+
+inventory = pd.read_csv('mbs_servicetrucks_01.csv')
+inventory['Storeroom Record ID'] = 'a0O1M00000sCL52UAG'
