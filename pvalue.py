d409620230df15032eb57e5f00778f19281f4624
tahentx <hendricks.ta@gmail.com>
2019-08-18 09:28:40 -0700

    create function to randomly pull element from list. first commit on this particular exercise

---
+++ pvalue.py
@@ -0,0 +1,8 @@
+
+import random
+researcher = ['Grant','Emelia','Jackson','Rochelle', 'Renee', 'Krystal','Monica','Abdul','Usama','Lee','Sung']
+
+def name(cohort: list) -> str:
+    print(random.choice(cohort))
+
+name(researcher)
