9ca1557f46ce1dca70764b0410810371dd249d0d
tahentx <hendricks.ta@gmail.com>
2019-08-18 14:03:58 -0700

    create random sentences drawing from researcher topic and purpose lists

--- pvalue.py
+++ pvalue.py
@@ -1,8 +1,13 @@
 
 import random
 researcher = ['Grant','Emelia','Jackson','Rochelle', 'Renee', 'Krystal','Monica','Abdul','Usama','Lee','Sung']
+a = random.choice(researcher)
+topic = ['biomedical','forensic accounting','particle physics','marine biology','polymer corrosion', 'voltage imbalance']
+b = random.choice(topic)
+purpose = ['research grant', 'prototype', 'side project']
+c = random.choice(purpose)
 
-def name(cohort: list) -> str:
-    print(random.choice(cohort))
+def sentence_structure(person, subject, goal):
+    print(person + " concluded a " + subject + " study to support a " + goal + ".")
 
-name(researcher)
+sentence_structure(a,b,c)
