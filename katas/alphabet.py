1f204a4e8b526b1d2c584ae64df2aebbab613b81
tahentx <hendricks.ta@gmail.com>
2019-08-24 11:35:05 -0700

    match letter with indices

---
+++ katas/alphabet.py
@@ -0,0 +1,6 @@
+# codewars kata alphabet replacement: https://www.codewars.com/kata/546f922b54af40e1e90001da/train/python
+letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+place = []
+for i in letter:
+    place.append(letter.index(i))
+assert len(place) == 26
