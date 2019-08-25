a12403adf948e8891273060bb9be6d7184836753
tahentx <hendricks.ta@gmail.com>
2019-08-25 15:20:39 -0700

    working solution in need of refactoring

--- katas/alphabet.py
+++ katas/alphabet.py
@@ -1,8 +1,11 @@
 # codewars kata alphabet replacement: https://www.codewars.com/kata/546f922b54af40e1e90001da/train/python
 letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+submission = input("Provide a letter: ")
 place = []
 for i in letter:
     place.append(letter.index(i))
 assert len(place) == 26
 x = dict(zip(letter,place))
-print(x)
+for i in letter:
+    if submission == i:
+        print(letter.index(i))
