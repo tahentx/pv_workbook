54f3707ac977da0a67ae53dd6e97d806ee9ff8a6
tahentx <hendricks.ta@gmail.com>
2019-08-26 12:39:48 -0700

    create empty list and append the index to the list

--- katas/alphabet.py
+++ katas/alphabet.py
@@ -2,6 +2,9 @@
 letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 submission = input("Provide text input: ")
 copy = list(submission)
-for x in submission:
+output = []
+for x in copy:
     if x in letter:
-        print(letter.index(x))
+        output.append(letter.index(x))
+
+print(output)
