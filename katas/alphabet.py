d819f8fee76d81d0e454c75370a10e50d5f09e38
tahentx <hendricks.ta@gmail.com>
2019-08-26 12:50:14 -0700

    complete solution with list comprehension and wrapping the logic in a function

--- katas/alphabet.py
+++ katas/alphabet.py
@@ -1,10 +1,9 @@
 # codewars kata alphabet replacement: https://www.codewars.com/kata/546f922b54af40e1e90001da/train/python
-letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-submission = input("Provide text input: ")
-copy = list(submission)
-output = []
-for x in copy:
-    if x in letter:
-        output.append(letter.index(x))
-
-print(output)
+def alphabet_postion(submission):
+    letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    submission = input("Provide text input: ")
+    copy = list(submission)
+    output = []
+    text = [output.append(letter.index(x)) for x in copy if x in letter]
+    string_output = str(output)
+    print(string_output)
