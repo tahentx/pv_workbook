2c0246b61f95c648eb985c571461c44e0fc25695
tahentx <hendricks.ta@gmail.com>
2019-09-02 11:45:37 -0700

    still need to return the unique pair only. taking a break, but more work to do.

--- katas/sum_pairs.py
+++ katas/sum_pairs.py
@@ -2,7 +2,7 @@
 def sum_pairs(int: list, s: int) -> list:
     y = []
     z = [y.append(s - x) for x in int]
-    for i,j in zip(y, int):
-        if i + j == s:
+    for i,j in zip(int,y):
+        if i + j == s and i != j:
             print(i,j)
 sum_pairs([5,8,2,4],10)
