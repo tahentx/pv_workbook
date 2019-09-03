c7394e365d4f1232e927225ebc634c35407f895b
tahentx <hendricks.ta@gmail.com>
2019-09-03 16:52:02 -0700

    separate the odd and the even elements of the list; sort the odd elements in preparation for reintegration.

--- katas/sort_odd.py
+++ katas/sort_odd.py
@@ -1,7 +1,9 @@
 # codewars kata: https://www.codewars.com/kata/sort-the-odd/train/python
 def sort_array(source_array):
+  odd_array = []
   for i in source_array:
       if i % 2 > 0:
-          source_array.insert(0,i)
-  print(source_array)
-sort_array([4,2,6,3,9])
+          odd_array.append(i)
+  odd_array.sort()
+  print(odd_array)
+sort_array([4,2,9,17,3,1])
