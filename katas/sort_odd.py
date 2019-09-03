e4863dc2eb2cc3afca1d0168f18423632b2a8f60
tahentx <hendricks.ta@gmail.com>
2019-09-03 13:35:07 -0700

    move the odd values to the front of the list

--- katas/sort_odd.py
+++ katas/sort_odd.py
@@ -1,12 +1,7 @@
 # codewars kata: https://www.codewars.com/kata/sort-the-odd/train/python
 def sort_array(source_array):
-  even_list = []
   for i in source_array:
-      if i % 2 == 0:
-          even_list.append(i)
-      else:
-          print("odd")
-  print(even_list)
+      if i % 2 > 0:
+          source_array.insert(0,i)
   print(source_array)
-
 sort_array([4,2,6,3,9])
