6b7d5b57184cdbc2784ab29c0b676459d760ab48
tahentx <hendricks.ta@gmail.com>
2019-09-15 20:13:17 -0700

    add example code from sklearn docs

---
+++ skl.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+X = np.array([[1,1],[1,2],[2,2],[2,3]])
+y = np.dot(X,np.array([1,2])) + 3
+reg = LinearRegression().fit(X, y)
+reg.score(X, y)
+print(reg.coef_)
+print(reg.intercept_)
