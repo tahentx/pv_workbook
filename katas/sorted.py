9c23ceb04da9c14d9d164e9760fe3c852a62f842
tahentx <hendricks.ta@gmail.com>
2019-09-18 15:54:21 -0700

    create logic to identify sort ascending

--- katas/sorted.py
+++ katas/sorted.py
@@ -1,6 +1,13 @@
 # Codewars kata: https://www.codewars.com/kata/sorted-yes-no-how/train/python
 def is_sorted_and_how(arr):
-  for i in arr:
-      print(i)
+    copy = arr.copy()
+    arr.sort()
+    if copy == arr:
+        print("Sorted already")
+    else:
+        print("Either descending or other")
 
-is_sorted_and_how([14,55])
+
+
+
+is_sorted_and_how([63,60,32])
