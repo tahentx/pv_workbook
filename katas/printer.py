88c2e61cad5a6168b66e37c642f4b3eee49e3463
tahentx <hendricks.ta@gmail.com>
2019-09-19 04:56:23 -0700

    working copy

--- katas/printer.py
+++ katas/printer.py
@@ -4,5 +4,10 @@ def printer_error(s):
   errors = []
   colors = ["a","b","c","d","e","f","g","h","i","j","k","l","m"]
   get_errors = [errors.append(x) for x in s if x not in colors]
-  
+  count = 0
+  for y in s:
+      if y in colors:
+          count = count + 1
+  total = len(errors) + count
+  print("Error rate: " + str(count) + "/" + str(total))
 printer_error("boo")
