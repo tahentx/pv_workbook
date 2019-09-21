9d87623bede122030f2eac8d58c358afaf3c3ba4
tahentx <hendricks.ta@gmail.com>
2019-09-21 07:35:46 -0700

    count number of occurences of digit in list, but still need to do logic to check number in multiple digit integers

--- katas/dig.py
+++ katas/dig.py
@@ -3,5 +3,9 @@ def nb_dig(n, d):
     while n > 0:
         a.append(n ** 2)
         n = n - 1
-    print(a)
-nb_dig(5,8)
+    count = 0
+    for i in a:
+        if i == d:
+            count = count + 1
+    print(count)
+nb_dig(5,44)
