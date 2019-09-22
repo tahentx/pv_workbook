4dd2b7cdef1d08ab3fa11a04c2b5b933c53b56c2
tahentx <hendricks.ta@gmail.com>
2019-09-22 13:19:10 -0700

    create data from article as dictionaries

---
+++ p_value.py
@@ -0,0 +1,14 @@
+# code inspired by medium article: https://medium.com/analytics-vidhya/everything-you-should-know-about-p-value-from-scratch-for-data-science-f3c0bfa3c4cc
+
+before_experiment = {
+  "years": 10,
+  "mean": 74,
+  "st_dev": 8,
+  "observations": 1000,
+}
+after_experiment = {
+  "years": 1,
+  "mean": 78,
+  "st_dev": 5,
+  "observations": 60,
+}
