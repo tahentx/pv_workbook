7ffb9aa0ae93e642e4f3e7dc8c2da33837285a15
tahentx <hendricks.ta@gmail.com>
2019-09-30 15:15:09 -0700

    answer for submission

--- katas/duplicate.py
+++ katas/duplicate.py
@@ -2,8 +2,10 @@ def duplicate(word):
     word2 = list(word)
     word3 = []
     for x in word2:
-        word3.append(x)
-        if word2.count(x) > 1:
-            word3.append(x)
-    print(word3)
-duplicate("Hell")
+        if word.count(x) > 1:
+            word3.append(")")
+        else:
+            word3.append("(")
+    answer = ''.join(word3)
+    return answer
+duplicate("Ball")
