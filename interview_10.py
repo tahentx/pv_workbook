c68ac8c08a10a0643643eefd41bc73665e1d8e93
tahentx <hendricks.ta@gmail.com>
2019-11-17 06:44:57 -0800

    final working solution, corrected minval variable from min_value to minval

--- interview_10.py
+++ interview_10.py
@@ -1,6 +1,6 @@
 
-def min_value(q):
-  j = 3
+def min_value(q, j=3):
   foo = {x: abs(x - j) for x in q}
-  print(min(foo, key=foo.get))
+  minval = min(foo.values())
+  return [k for k,v in foo.items() if v == minval]
 min_value([4,2,5,7,6])
