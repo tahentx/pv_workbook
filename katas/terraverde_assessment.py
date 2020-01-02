1c23fc0d1258ea5e8d65ccd145047c0078f3ff47
tahentx <hendricks.ta@gmail.com>
2020-01-02 11:37:52 -0800

    create perpendicular vector

--- katas/terraverde_assessment.py
+++ katas/terraverde_assessment.py
@@ -11,17 +11,22 @@
 # create_intersection([[5,4],[7,5]])
 
 def lineFromPoints(P,Q):
-
+    # The first step is to calculate the slope of the line
     a = Q[1] - P[1]
     b = P[0] - Q[0]
     c = a*(P[0]) + b*(P[1])
 
-    if(b<0):
-        print("The line passing through points P and Q is:",
-              a ,"x ",b ,"y = ",c ,"\n")
-    else:
-        print("The line passing through points P and Q is: ",
-              a ,"x + " ,b ,"y = ",c ,"\n")
+    # Next step is to create a perpendicular line
+    int_vector = -1/c
+    print(str(int_vector))
+
+    #
+    # if(b<0):
+    #     print("The line passing through points P and Q is:",
+    #           a ,"x ",b ,"y = ",c ,"\n")
+    # else:
+    #     print("The line passing through points P and Q is: ",
+    #           a ,"x + " ,b ,"y = ",c ,"\n")
 
 # Driver code
 if __name__=='__main__':
