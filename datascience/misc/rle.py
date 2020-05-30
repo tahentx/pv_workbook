1f3c886607e4e99d819a3205109ef80c51d5e31f
Todd Hendricks <hendricks.ta@gmail.com>
2020-05-29 21:58:37 -0700

    complete kata

--- datascience/misc/rle.py
+++ datascience/misc/rle.py
@@ -1,17 +1,35 @@
 # kata: https://www.codewars.com/kata/57d6c3fb950d84fcfb0015c8/train/python
-from itertools import groupby
-def encode(input):
-    freq = [len(list(group)) for key, group in groupby(input)]
-    encoded = zip(list(set(input)),freq)
-    output = []
-    for char in list(input):
-        for c in encoded:
-            if char == c[0]:
-                output.append(char + str(c[1]))
-    output = list(set(output))
-    output = ''.join(output)
-    return output
+# from itertools import groupby
+# def encode(input):
+#     freq = [len(list(group)) for key, group in groupby(input)]
+#     encoded = zip(list(set(input)),freq)
+#     output = []
+#     for char in list(input):
+#         for c in encoded:
+#             if char == c[0]:
+#                 output.append(char + str(c[1]))
+#     output = list(set(output))
+#     output = ''.join(output)
+#     return output
     
     
     
-encode("AAAALOTOOOOFTEEEEXXXXXXT")
+# encode("AAAALOTOOOOFTEEEEXXXXXXT")
+vows = {
+    'a' : 1,
+    'e' : 2,
+    'i' : 3,
+    'o' : 4,
+    'u' : 5,
+}
+
+def encode(st):
+    st = list(st)
+    output = []    
+    for val in st:
+        if val in vows.keys():
+            output.append(vows.get(val))
+        else:
+            output.append(val)
+    print(output)
+encode('hello')
