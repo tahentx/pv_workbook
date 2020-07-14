f13c566e8ba7ec3fafb6965e2c4a6c266c9c2908
Todd Hendricks <hendricks.ta@gmail.com>
2020-07-14 10:42:26 -0700

    working solution to question 112.

---
+++ datascience/misc/ds_112.py
@@ -0,0 +1,13 @@
+# Data Science Interview Question #112
+
+# Given an array of n distinct elements, can you find all the triplet combinations that sum to zero?
+
+from itertools import combinations
+
+def sum_triplet_to_zero(input):
+    base = combinations(input,3)
+    for x in base:
+        if sum(x) == 0:
+            print(x)
+
+sum_triplet_to_zero([5,-1,-4,9,3])
