eb1f5b888b278ac9bd83fd20c04b4cad49a32e17
Todd Hendricks <hendricks.ta@gmail.com>
2020-07-14 10:45:43 -0700

    refactor to use filter for aesthetic improvement. this change removes a line of code and is more elegant.

--- datascience/misc/ds_112.py
+++ datascience/misc/ds_112.py
@@ -6,8 +6,10 @@ from itertools import combinations
 
 def sum_triplet_to_zero(input):
     base = combinations(input,3)
-    for x in base:
-        if sum(x) == 0:
-            print(x)
+    zero_sum = filter(lambda x: sum(x) == 0, base)
+    print(zero_sum)
+    # for x in base:
+    #     if sum(x) == 0:
+    #         print(x)
 
 sum_triplet_to_zero([5,-1,-4,9,3])
