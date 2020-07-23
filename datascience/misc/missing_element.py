5fd63b565067156926f70aaec27656f04f01b02e
Todd Hendricks <hendricks.ta@gmail.com>
2020-07-23 14:56:08 -0700

    solution to question 104

--- datascience/misc/missing_element.py
+++ datascience/misc/missing_element.py
@@ -10,4 +10,15 @@ def find_missing_element(a, n):
     c = [x for x in b if x not in a]
     z = c[n - 1]
     return z
-find_missing_element([2, 3, 11, 9],3)
+# find_missing_element([2, 3, 11, 9],3)
+
+def alternating_array(arr):
+    new_arr = []
+    length = len(arr)
+    while len(new_arr) <= length:
+        low, high = min(arr), max(arr)
+        new_arr.append(high); new_arr.append(low)
+        arr = sorted(arr)[1:-1]
+    return set(new_arr)
+
+alternating_array([10, 2, 11, 3, 7, 4, 1])
