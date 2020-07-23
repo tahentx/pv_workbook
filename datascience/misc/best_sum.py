8a6a91ef918860ec2edbef9c3e2544f0db423927
Todd Hendricks <hendricks.ta@gmail.com>
2020-07-23 14:01:30 -0700

    refactored to handle case in which there is no max.

--- datascience/misc/best_sum.py
+++ datascience/misc/best_sum.py
@@ -3,6 +3,6 @@ def get_best_distances_sum(t, k, ls):
     options = list(combinations(ls, k))
     totals = [sum(x) for x in options]
     less_than_t = list(filter(lambda x: x <= t, totals))
-    return max(less_than_t)
-
+    best_option = max(less_than_t) if less_than_t else None
+    return best_option
 get_best_distances_sum(8,3,[1, 2, 3, 4])
