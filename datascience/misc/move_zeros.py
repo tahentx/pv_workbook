18a743a0572afa477d1b6e809c1dff350aa8d9fb
Todd Hendricks <hendricks.ta@gmail.com>
2020-07-27 09:11:52 -0700

    built basic pig lation word

--- datascience/misc/move_zeros.py
+++ datascience/misc/move_zeros.py
@@ -10,8 +10,11 @@ def move_zeros(array):
             non_zeros.append(x)
     non_zeros.extend(zeros)
     return non_zeros
-    
 
-        
+# move_zeros([False,1,0,1,2,0,1,3,"a"])
 
-move_zeros([False,1,0,1,2,0,1,3,"a"])
+def pig_latin(text):
+    front, base = text[0], text[1:]
+    new_word = base + front + 'ay'
+    print(new_word)
+pig_latin("hello")
