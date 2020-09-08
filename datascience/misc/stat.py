cc7e2e2992f7cc9020f48339a793f192e3af5753
Todd Hendricks <hendricks.ta@gmail.com>
2020-09-08 15:03:46 -0700

    get basic structure down

--- datascience/misc/stat.py
+++ datascience/misc/stat.py
@@ -1,9 +1,13 @@
 # https://www.codewars.com/kata/5878520d52628a092f0002d0/train/python
-def string_transformer(s):
-    if not s:
-        return ''
-    else:
-        s = s.split()
-        return s
+# def string_transformer(s):
+#     if not s:
+#         return ''
+#     else:
+#         s = s.split()
+#         return s
     
-string_transformer("Example string")
+# string_transformer("Example string")
+
+def name_value(my_list):
+    z = sum([my_list.index(x) + 1 for x in my_list])
+    return z
