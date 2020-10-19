fa348fd6e4e073d5eecc7fb6db1e953d6aa329fd
Todd Hendricks <hendricks.ta@gmail.com>
2020-10-19 10:14:46 -0700

    identify repeat values in list through comparing it to expected values

--- datascience/misc/autocomplete.py
+++ datascience/misc/autocomplete.py
@@ -1,5 +1,21 @@
 def autocomplete(input_, dictionary):
     length = len(input_)
+    for x in dictionary:
+        if x.isalpha() == True:
+            pass
+        else:
+            x = ''
     answer = [word for word in dictionary if word[:length] == input_]
-    return answer
-autocomplete('ai', ['airplane','airport','apple','ball'])
+    print(answer)
+# autocomplete('ai', ['airplane','airport','apple','ball'])
+
+def duplicate_sandwich(arr):
+    base = range(0, len(arr))
+    joined = zip([arr.index(a) for a in arr], base)
+    for member in joined:
+        if member[0] != member[1]:
+            front, back = member[0], member[1]
+            print(arr[front])
+            print((arr[back]))
+
+duplicate_sandwich(["hi","hey", "hello","howdy","hey"])
