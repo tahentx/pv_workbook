4691b292c5b6d04394a99e2bc98b703dbf59b105
Todd Hendricks <hendricks.ta@gmail.com>
2020-10-20 14:50:11 -0700

    submitted draft of kata https://www.codewars.com/kata/59f11118a5e129e591000134/train/python

--- datascience/misc/autocomplete.py
+++ datascience/misc/autocomplete.py
@@ -14,8 +14,36 @@ def duplicate_sandwich(arr):
     joined = zip([arr.index(a) for a in arr], base)
     for member in joined:
         if member[0] != member[1]:
-            front, back = member[0], member[1]
-            print(arr[front])
-            print((arr[back]))
+            front, back = member[0] + 1, member[1]
+            slice = arr[front:back]
+            return slice
 
-duplicate_sandwich(["hi","hey", "hello","howdy","hey"])
+# duplicate_sandwich(["hi","hey", "hello","howdy","hey"])
+from itertools import groupby
+from operator import itemgetter
+def remove_duplicate_words(s):
+   answer = ' '.join(map(itemgetter(0), groupby(s.split())))
+   print(answer)
+# remove_duplicate_words("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta")
+
+def remove_parentheses(s):
+    first = s.split('(')
+    last = s[::-1]
+    end_last = last.split(')')
+    product = ''.join([first[0], end_last[0][::-1]])
+    return product
+
+remove_parentheses("hello(exm(p)l)heyoo")
+
+from collections import Counter
+def repeats(arr):
+    count = Counter()
+    for item in arr:
+        count[item] += 1
+    singles = []
+    for key, value in count.items():
+        if value == 1:
+            singles.append(key)
+    return sum(singles)
+
+repeats([9, 10, 19, 13, 19, 13])
