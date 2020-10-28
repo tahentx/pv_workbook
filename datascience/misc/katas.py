d5b35a87fc7c9437ab56e22c6659ae42d8481bdf
Todd Hendricks <hendricks.ta@gmail.com>
2020-10-28 15:02:07 -0700

    combine characters as string

--- datascience/misc/katas.py
+++ datascience/misc/katas.py
@@ -1,6 +1,6 @@
-from collections import Counter
-from operator import itemgetter
 def string_letter_count(s):
+    from collections import Counter
+    from operator import itemgetter
     cnt = Counter()
     for word in s.lower():
         if word == ' ':
@@ -10,6 +10,12 @@ def string_letter_count(s):
     cnt = dict(cnt)
     sorted_cnt = sorted(cnt.items(), key=itemgetter(0))
     reordered = [x[::-1] for x in sorted_cnt]
-    print(reordered)
+    t = []
+    for x in reordered:
+        f = str(x[0])
+        t.append(f)
+        t.append(x[1])
+    answer = ''.join(t)
+    return answer
 
 string_letter_count("This is a test sentence")
