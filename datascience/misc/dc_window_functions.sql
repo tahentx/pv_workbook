a5115fdf1b77133fc1ce6ffdb961d3828019618d
Todd Hendricks <hendricks.ta@gmail.com>
2020-12-03 12:00:22 -0800

    capture reigning weightlifting champions

---
+++ datascience/misc/dc_window_functions.sql
@@ -0,0 +1,19 @@
+WITH Weightlifting_Gold AS (
+  SELECT
+    -- Return each year's champions' countries
+    Year,
+    Country AS champion
+  FROM Summer_Medals
+  WHERE
+    Discipline = 'Weightlifting' AND
+    Event = '69KG' AND
+    Gender = 'Men' AND
+    Medal = 'Gold')
+
+SELECT
+  Year, Champion,
+  -- Fetch the previous year's champion
+  LAG(Champion) OVER
+    (ORDER BY Champion ASC) AS Last_Champion
+FROM Weightlifting_Gold
+ORDER BY Year ASC;
