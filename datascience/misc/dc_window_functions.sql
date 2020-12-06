f99f1494ad8d52d850808e52d39bf2c53228131b
Todd Hendricks <hendricks.ta@gmail.com>
2020-12-06 13:11:05 -0800

    practice moving averages on olypmics dataset

--- datascience/misc/dc_window_functions.sql
+++ datascience/misc/dc_window_functions.sql
@@ -35,3 +35,22 @@ SELECT
                          ORDER BY Year ASC) AS Last_Champion
 FROM Tennis_Gold
 ORDER BY Gender ASC, Year ASC;
+
+-- Calculate the 3-year moving sum of medals earned per country.
+
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Year, Country)
+
+SELECT
+  Year, Country, Medals,
+  -- Calculate each country's 3-game moving total
+  SUM(Medals) OVER
+    (PARTITION BY Country
+     ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
--- datascience/misc/rp_stringformat.py
+++ datascience/misc/rp_stringformat.py
@@ -1,5 +1,10 @@
 # print("Hello".format)
-two = "Hello, {}".format("Todd")
-three = "{} {} {}".format("Yo", "hi", "what up")
-four = "{2} {0} {1}".format("Yo", "hi", "what up")
-five = "{a} {b} {c}".format(a="Joe", b="Chuck", c="Steve")
+# two = "Hello, {}".format("Todd")
+# three = "{} {} {}".format("Yo", "hi", "what up")
+# four = "{2} {0} {1}".format("Yo", "hi", "what up")
+# five = "{a} {b} {c}".format(a="Joe", b="Chuck", c="Steve")
+
+import sys
+
+print("Name of the script : {}".format(sys.argv[0]))
+print("Arguments of the script : {}".format(sys.argv[1:]))
