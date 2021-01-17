5266ad0069df3c26d796bbc81d6c85048a9c8171
Todd Hendricks <hendricks.ta@gmail.com>
2021-01-16 16:58:14 -0800

    practice: prepend new team to list

--- datascience/misc/twentyone.scala
+++ datascience/misc/twentyone.scala
@@ -2,5 +2,6 @@ val twoClubs: Int = 2
 val threeClubs: Int = 3
 val fourClubs: Int = 4
 
-val colors = Array("Red", "yellow", "blue")
-colors.foreach(println)
+val teams = List("hawks", "devils", "jays", "warriors")
+val newTeam = "dolphins" :: teams
+println(newTeam)
